f1da542287e64d55534bc037698ad579bba97d49
Doron Epstein <jimbeanzn@gmail.com>
2016-07-17 19:17:51 +0200

    feat: setup projects
    add all possibly required basic projects
    work on same small interface features

---
+++ HulkOut.Interfaces/DataAccess/ICacheService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HulkOut.Interfaces.DataAccess
+{
+	public interface ICacheService
+	{
+		T Get<T>(string key);
+		T Set<T>(string key, T value);
+		bool Delete(string key);
+		bool Exists(string key);
+
+		bool FlushAll();
+		bool FlushCurrent();
+	}
+}
