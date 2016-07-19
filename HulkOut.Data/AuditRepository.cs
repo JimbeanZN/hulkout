2243e7d4a3a87618bf07b1379bd57fd121d61568
Doron Epstein <jimbeanzn@gmail.com>
2016-07-19 22:39:41 +0200

    feat: add entity framework
    add ef to new data.ef project
    add dbcontext

---
+++ HulkOut.Data/AuditRepository.cs
@@ -0,0 +1,34 @@
+﻿using HulkOut.Interfaces.DataAccess.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HulkOut.Models.Data;
+using System.Linq.Expressions;
+using static HulkOut.Models.Enums.AuditEnums;
+
+namespace HulkOut.Data.EF
+{
+	public class AuditRepository : IAuditRepository
+	{
+		public Audit Create(Audit audit)
+		{
+			using (var db = new HulkOutDbContext())
+			{
+				db.Audits.Add(audit);
+				db.SaveChanges();
+
+				return audit;
+			}
+		}
+
+		public IEnumerable<Audit> Get(Expression<Func<Audit, bool>> filter)
+		{
+			using (var db = new HulkOutDbContext())
+			{
+				return db.Audits.Where(filter).ToList();
+			}
+		}
+	}
+}
---
+++ HulkOut.Data/HulkOutDbContext.cs
@@ -0,0 +1,30 @@
+﻿using HulkOut.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HulkOut.Data.EF
+{
+	public class HulkOutDbContext : DbContext
+	{
+		public HulkOutDbContext() : base("HulkOutDbConnectionString")
+		{
+
+		}
+
+		public DbSet<Audit> Audits { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Timer> Timers { get; set; }
+		public DbSet<User> Users { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			//Configure domain classes using Fluent API here
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
