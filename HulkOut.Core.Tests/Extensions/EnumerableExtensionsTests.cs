2d68cf452b7edc997b4377e2ee2dddec5ef6bed7
Doron Epstein <jimbeanzn@gmail.com>
2016-07-27 20:30:09 +0200

    test: added test solution for core
    added unit tests for extension method
    tdd ftw

---
+++ HulkOut.Core.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using HulkOut.Core.Extensions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HulkOut.Core.Tests.Extensions
+{
+	[TestFixture]
+	public class EnumerableExtensionsTests
+	{
+		[Test]
+		public void IsNotNullAndEmpty_GivenNullEnumerable_ReturnsFalse()
+		{
+			//arrange
+			IEnumerable<string> list = null;
+
+			//act
+			var result = list.IsNotNullAndEmpty();
+
+			//assert
+			Assert.IsFalse(result);
+		}
+
+		[Test]
+		public void IsNotNullAndEmpty_GivenEmptyEnumerable_ReturnsFalse()
+		{
+			//arrange
+			var list = new List<string>();
+
+			//act
+			var result = list.IsNotNullAndEmpty();
+
+			//assert
+			Assert.IsFalse(result);
+		}
+
+		[Test]
+		public void IsNotNullAndEmpty_GivenEnumerableItems_ReturnsTrue()
+		{
+			//arrange
+			var list = new List<string> { "Item 1" };
+
+			//act
+			var result = list.IsNotNullAndEmpty();
+
+			//assert
+			Assert.IsTrue(result);
+		}
+	}
+}
