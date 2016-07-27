c0ccc9cccd10771ff628994d1ab6d4c4dfe96449
Doron Epstein <jimbeanzn@gmail.com>
2016-07-27 19:57:50 +0200

    feat: is null or empty extension
    create an extension that checks for null or a list having items

---
+++ HulkOut.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HulkOut.Core.Extensions
+{
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		/// Determines whether a sequence is not null or empty.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <returns></returns>
+		public static bool IsNotNullOrEmpty<T>(this IEnumerable<T> source)
+		{
+			return source != null && source.Any();
+		}
+	}
+}
