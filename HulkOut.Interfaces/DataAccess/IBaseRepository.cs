6fc6d700f68118eadead6b5cf1baf84e6186043e
Doron Epstein <jimbeanzn@gmail.com>
2016-07-30 14:12:55 +0200

    feat: base repo and service interfaces
    add base repo interface
    add base service interface

---
+++ HulkOut.Interfaces/DataAccess/IBaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HulkOut.Models;
+
+namespace HulkOut.Interfaces.DataAccess
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IBaseRepository<T> where T : BaseModel
+	{
+		/// <summary>
+		/// Gets the specified filter.
+		/// </summary>
+		/// <param name="filter">The filter.</param>
+		/// <returns></returns>
+		IEnumerable<T> Get(Expression<Func<T, bool>> filter);
+
+		/// <summary>
+		/// Inserts the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		T Insert(T model);
+
+		/// <summary>
+		/// Updates the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		T Update(T model);
+
+		/// <summary>
+		/// Deletes the specified identifier.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns></returns>
+		bool Delete(Guid id);
+	}
+}
---
+++ HulkOut.Interfaces/DataAccess/IBaseService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using HulkOut.Models;
+
+namespace HulkOut.Interfaces.DataAccess
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IBaseService<T> where T : BaseModel
+	{
+		/// <summary>
+		/// Gets the specified identifier.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns></returns>
+		T Get(Guid id);
+
+		/// <summary>
+		/// Get all by the specified filter.
+		/// </summary>
+		/// <param name="filter">The filter.</param>
+		/// <returns></returns>
+		IEnumerable<T> GetAll(Expression<Func<T, bool>> filter);
+
+		/// <summary>
+		/// Inserts the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		T Insert(T model);
+
+		/// <summary>
+		/// Updates the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		T Update(T model);
+
+		/// <summary>
+		/// Deletes the specified identifier.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns></returns>
+		bool Delete(Guid id);
+	}
+}
