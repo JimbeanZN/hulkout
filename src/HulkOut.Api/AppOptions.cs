ad81d7544b515d414d8b790d5ddf435561e35854
Doron Epstein <jimbeanzn@gmail.com>
2017-07-04 11:16:52 +0200

    feat: added custom config section
    wire up new custom config section
    ensure swagger uses config
    refactor startup

---
+++ src/HulkOut.Api/AppOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace HulkOut.Api
+{
+	public class AppOptions
+	{
+		public string Version { get; set; }
+		public string ApplicationName { get; set; }
+		public string ApplicationDescription { get; set; }
+	}
+}
--- src/HulkOut.Api/Startup.cs
+++ src/HulkOut.Api/Startup.cs
@@ -9,6 +9,10 @@ namespace HulkOut.Api
 {
 	public class Startup
 	{
+		/// <summary>
+		///   Initializes a new instance of the <see cref="Startup" /> class.
+		/// </summary>
+		/// <param name="env">The hosting enviroment.</param>
 		public Startup(IHostingEnvironment env)
 		{
 			var builder = new ConfigurationBuilder()
@@ -19,27 +23,66 @@ namespace HulkOut.Api
 			Configuration = builder.Build();
 		}
 
+		/// <summary>
+		///   Gets the configuration.
+		/// </summary>
+		/// <value>
+		///   The configuration.
+		/// </value>
 		public IConfigurationRoot Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
+		/// <summary>
+		///   This method gets called by the runtime. Use this method to add services to the container.
+		/// </summary>
+		/// <param name="services">The services collection.</param>
 		public void ConfigureServices(IServiceCollection services)
 		{
-			// Add framework services.
+			var configurationSection = Configuration.GetSection("App");
+
+			ConfigureOptions(services, configurationSection);
+			ConfigureSwaggerGen(services, configurationSection);
+
 			services.AddMvc();
+		}
+
+		/// <summary>
+		///   Configures the swagger gen.
+		/// </summary>
+		/// <param name="services">The services collection.</param>
+		/// <param name="configurationSection">The configuration section.</param>
+		private static void ConfigureSwaggerGen(IServiceCollection services, IConfigurationSection configurationSection)
+		{
+			var appOptions = configurationSection.Get<AppOptions>();
 
 			services.AddSwaggerGen(options =>
 			{
 				options.SingleApiVersion(new Info
 				{
-					Version = "v1",
-					Title = "Auth0 Swagger Sample API",
-					Description = "API Sample made for Auth0",
+					Version = appOptions.Version,
+					Title = appOptions.ApplicationName,
+					Description = appOptions.ApplicationDescription,
 					TermsOfService = "None"
 				});
 			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// <summary>
+		///   Configures the application options.
+		/// </summary>
+		/// <param name="services">The services collection.</param>
+		/// <param name="configurationSection">The configuration section.</param>
+		private static void ConfigureOptions(IServiceCollection services, IConfigurationSection configurationSection)
+		{
+			services.AddOptions();
+			services.Configure<AppOptions>(configurationSection);
+		}
+
+		/// <summary>
+		///   This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// </summary>
+		/// <param name="app">The application.</param>
+		/// <param name="env">The env.</param>
+		/// <param name="loggerFactory">The logger factory.</param>
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
 			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
