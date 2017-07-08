dfc847ad29e77e0b6b7f204c6d1804d80f639de6
Doron Epstein <jimbeanzn@gmail.com>
2017-07-08 16:30:24 +0200

    feat: complete wiring up Category Service
    complete insert, update and delete commands with all tests

--- src/HulkOut.Logic/CategoryService.cs
+++ src/HulkOut.Logic/CategoryService.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using HulkOut.Shared.Interfaces.Categories;
 using HulkOut.Shared.Models.Data;
 
@@ -27,7 +25,6 @@ namespace HulkOut.Logic
 		/// </summary>
 		/// <param name="id">The identifier.</param>
 		/// <returns></returns>
-		/// <exception cref="NotImplementedException"></exception>
 		public Category Get(Guid id)
 		{
 			return GetAll(model => model.Id == id).FirstOrDefault();
@@ -39,7 +36,6 @@ namespace HulkOut.Logic
 		/// <param name="filter">The filter.</param>
 		/// <returns></returns>
 		/// <exception cref="ArgumentNullException">filter</exception>
-		/// <exception cref="NotImplementedException"></exception>
 		public IEnumerable<Category> GetAll(Expression<Func<Category, bool>> filter)
 		{
 			if (filter == null)
@@ -53,13 +49,13 @@ namespace HulkOut.Logic
 		/// </summary>
 		/// <param name="model">The model.</param>
 		/// <returns></returns>
-		/// <exception cref="NotImplementedException"></exception>
+		/// <exception cref="ArgumentNullException">model</exception>
 		public Category Insert(Category model)
 		{
 			if (model == null)
 				throw new ArgumentNullException(nameof(model));
 
-			throw new NotImplementedException();
+			return _categoryRepository.Insert(model);
 		}
 
 		/// <summary>
@@ -67,13 +63,13 @@ namespace HulkOut.Logic
 		/// </summary>
 		/// <param name="model">The model.</param>
 		/// <returns></returns>
-		/// <exception cref="NotImplementedException"></exception>
+		/// <exception cref="ArgumentNullException">model</exception>
 		public Category Update(Category model)
 		{
 			if (model == null)
 				throw new ArgumentNullException(nameof(model));
 
-			throw new NotImplementedException();
+			return _categoryRepository.Update(model);
 		}
 
 		/// <summary>
@@ -81,10 +77,9 @@ namespace HulkOut.Logic
 		/// </summary>
 		/// <param name="id">The identifier.</param>
 		/// <returns></returns>
-		/// <exception cref="NotImplementedException"></exception>
 		public bool Delete(Guid id)
 		{
-			throw new NotImplementedException();
+			return _categoryRepository.Delete(id);
 		}
 	}
 }
--- src/HulkOut.Tests/Logic/CategoryServiceTests.cs
+++ src/HulkOut.Tests/Logic/CategoryServiceTests.cs
@@ -21,6 +21,8 @@ namespace HulkOut.Tests.Logic
 			return categoryService;
 		}
 
+		#region Get
+
 		[TestMethod]
 		public void Get_GivenAGuid_ExpectAnObjectOfTypeCategory()
 		{
@@ -40,6 +42,10 @@ namespace HulkOut.Tests.Logic
 			Assert.IsInstanceOfType(result, typeof(Category));
 			Assert.AreEqual(guid1, result.Id);
 		}
+
+		#endregion
+
+		#region GetAll
 
 		[TestMethod]
 		public void GetAll_GivenNullFilter_ThrowsError()
@@ -62,7 +68,7 @@ namespace HulkOut.Tests.Logic
 			var categoryService = CategoryService();
 
 			_categoryRepository.Get(Arg.Any<Expression<Func<Category, bool>>>())
-				.Returns(new List<Category> { new Category(), new Category() });
+				.Returns(new List<Category> {new Category(), new Category()});
 
 			//act
 			var result = categoryService.GetAll(model => true);
@@ -73,6 +79,10 @@ namespace HulkOut.Tests.Logic
 			Assert.AreEqual(2, result.Count());
 			_categoryRepository.Received().Get(Arg.Any<Expression<Func<Category, bool>>>());
 		}
+
+		#endregion
+
+		#region Insert
 
 		[TestMethod]
 		public void Insert_GivenNullModel_ThrowsError()
@@ -89,6 +99,37 @@ namespace HulkOut.Tests.Logic
 		}
 
 		[TestMethod]
+		public void Insert_GivenAModel_ReturnsAModelWithAnId()
+		{
+			//arrange
+			var categoryService = CategoryService();
+
+			var passedModel = new Category
+			{
+				Title = "Test Category",
+				Description = "Test Category Description"
+			};
+			var expectedModel = passedModel;
+			expectedModel.Id = new Guid();
+
+			_categoryRepository.Insert(passedModel).Returns(expectedModel);
+
+			//act
+			var result = categoryService.Insert(passedModel);
+
+			//assert
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(Category));
+			Assert.AreEqual(expectedModel.Id, result.Id);
+			Assert.AreEqual(expectedModel.Title, result.Title);
+			Assert.AreEqual(expectedModel.Description, result.Description);
+		}
+
+		#endregion
+
+		#region Update
+
+		[TestMethod]
 		public void Update_GivenNullModel_ThrowsError()
 		{
 			//arrange
@@ -101,5 +142,59 @@ namespace HulkOut.Tests.Logic
 			Assert.IsNotNull(result);
 			Assert.AreEqual("model", result.ParamName);
 		}
+
+		[TestMethod]
+		public void Update_GivenAModel_ReturnsAnUpdatedModelWithUserAndDateTime()
+		{
+			//arrange
+			var categoryService = CategoryService();
+
+			var passedModel = new Category
+			{
+				Id = new Guid(),
+				Title = "Test Category",
+				Description = "Test Category Description",
+				LastUpdatedByUserId = new Guid(),
+				LastUpdatedDate = DateTime.Now.AddDays(-1)
+			};
+			var expectedModel = passedModel;
+			expectedModel.LastUpdatedByUserId = new Guid();
+			expectedModel.LastUpdatedDate = expectedModel.LastUpdatedDate.AddDays(1);
+
+			_categoryRepository.Update(passedModel).Returns(expectedModel);
+
+			//act
+			var result = categoryService.Update(passedModel);
+
+			//assert
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(Category));
+			Assert.AreEqual(expectedModel.Id, result.Id);
+			Assert.AreEqual(expectedModel.LastUpdatedDate, result.LastUpdatedDate);
+			Assert.AreEqual(expectedModel.LastUpdatedByUserId, result.LastUpdatedByUserId);
+		}
+
+		#endregion
+
+		#region Delete
+
+		[TestMethod]
+		public void Delete_GivenAGuid_ReturnsTrueIfSuccessful()
+		{
+			//arrange
+			var categoryService = CategoryService();
+
+			_categoryRepository.Delete(Arg.Any<Guid>()).Returns(true);
+
+			//act
+			var result = categoryService.Delete(Arg.Any<Guid>());
+
+			//assert
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(bool));
+			Assert.IsTrue(result);
+		}
+
+		#endregion
 	}
 }
