d5437580f9872e631990a4cdb45f2dc1bc14042c
Doron Epstein <jimbeanzn@gmail.com>
2017-12-25 16:12:20 +0200

    feat: add base crud service
    add an interface to standardize a crud service

---
+++ src/HulkOut/Interfaces/IBaseApiCrudService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Http;
+
+namespace HulkOut.Interfaces
+{
+	/// <summary>
+	/// An interface that can be used to baseline API CRUD services.
+	/// </summary>
+	/// <typeparam name="TId">The type of the Id identifier.</typeparam>
+	/// <typeparam name="TModel">The type of the model.</typeparam>
+	public interface IBaseApiCrudService<in TId, in TModel>
+	{
+		/// <summary>
+		/// Get all instances.
+		/// </summary>
+		/// <returns></returns>
+		IHttpActionResult Get();
+
+		/// <summary>
+		/// Gets the specified instance.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns></returns>
+		IHttpActionResult Get(TId id);
+
+		/// <summary>
+		/// Posts the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		IHttpActionResult Post(TModel model);
+
+		/// <summary>
+		/// Updates the specified model.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		IHttpActionResult Put(TId id, TModel model);
+
+		/// <summary>
+		/// Deletes the specified model.
+		/// </summary>
+		/// <param name="model">The model.</param>
+		/// <returns></returns>
+		IHttpActionResult Delete(TId model);
+	}
+}
