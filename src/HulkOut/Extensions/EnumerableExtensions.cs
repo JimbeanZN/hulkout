7d755feac20f2191cbfa4c9154c45d94cc35176f
Doron Epstein <jimbeanzn@gmail.com>
2018-01-28 15:08:41 +0200

    chore: code cleanup
    resharper code cleanup

--- src/HulkOut/Extensions/EnumerableExtensions.cs
+++ src/HulkOut/Extensions/EnumerableExtensions.cs
@@ -8,14 +8,11 @@ namespace HulkOut.Extensions
 	public static class EnumerableExtensions
 	{
 		/// <summary>
-		///   Determines whether a sequence is null or empty.
+		///   Determines whether a sequence <see cref="IEnumerable{T}" /> is null or empty.
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="source">The source.</param>
 		/// <returns></returns>
-		public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
-		{
-			return source == null || !source.Any();
-		}
+		public static bool IsNullOrEmpty<T>(this IEnumerable<T> source) => source == null || !source.Any();
 	}
 }
