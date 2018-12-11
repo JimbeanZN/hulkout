e707e40321c77a127c28c95f6dddf713fba81dae
Doron Epstein <jimbeanzn@gmail.com>
2018-12-11 19:02:02 +0200

    feat: add required query param code

---
+++ src/HulkOut.AspNetCore/Actions/Constraints/RequiredFromQueryActionConstraint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace HulkOut.AspNetCore.Actions.Constraints
+{
+  /// <summary>
+  /// Credit: https://www.strathweb.com/2016/09/required-query-string-parameters-in-asp-net-core-mvc/
+  /// </summary>
+  public class RequiredFromQueryActionConstraint : IActionConstraint
+  {
+    private readonly string _parameter;
+ 
+    public RequiredFromQueryActionConstraint(string parameter)
+    {
+      _parameter = parameter;
+    }
+ 
+    public int Order => 999;
+ 
+    public bool Accept(ActionConstraintContext context)
+    {
+      return context.RouteContext.HttpContext.Request.Query.ContainsKey(_parameter);
+    }
+  }
+}
---
+++ src/HulkOut.AspNetCore/Attributes/RequiredFromQueryAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using HulkOut.AspNetCore.Actions.Constraints;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace HulkOut.AspNetCore.Attributes
+{
+  /// <summary>
+  /// Credit: https://www.strathweb.com/2016/09/required-query-string-parameters-in-asp-net-core-mvc/
+  /// </summary>
+  public class RequiredFromQueryAttribute : FromQueryAttribute, IParameterModelConvention
+  {
+    public void Apply(ParameterModel parameter)
+    {
+      if (parameter.Action.Selectors != null && parameter.Action.Selectors.Any())
+      {
+        parameter.Action.Selectors.Last().ActionConstraints.Add(new RequiredFromQueryActionConstraint(parameter.BindingInfo?.BinderModelName ?? parameter.ParameterName));
+      }
+    }
+  }
+}
