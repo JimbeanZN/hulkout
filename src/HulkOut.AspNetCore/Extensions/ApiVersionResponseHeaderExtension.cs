c047e5499edcc6ad65f2d682339d9246a8a7c97c
Doron Epstein <jimbeanzn@gmail.com>
2018-12-29 14:53:08 +0200

    feat: api version response header middleware

---
+++ src/HulkOut.AspNetCore/Extensions/ApiVersionResponseHeaderExtension.cs
@@ -0,0 +1,14 @@
+﻿using HulkOut.AspNetCore.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace HulkOut.AspNetCore.Extensions
+{
+  public static class ApiVersionResponseHeaderExtension
+  {
+    public static IApplicationBuilder UseApiVersionResponse(
+      this IApplicationBuilder builder)
+    {
+      return builder.UseMiddleware<ApiVersionResponseHeaderMiddleware>();
+    }
+  }
+}
---
+++ src/HulkOut.AspNetCore/Middleware/ApiVersionResponseHeaderMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HulkOut.AspNetCore.Middleware
+{
+  public class ApiVersionResponseHeaderMiddleware
+  {
+    private readonly RequestDelegate _next;
+
+    public ApiVersionResponseHeaderMiddleware(RequestDelegate next)
+    {
+      _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+      context.Response.OnStarting(state =>
+      {
+        context.Response.Headers.Add("X-Api-Version", Assembly.GetEntryAssembly().GetName().Version.ToString());
+        return Task.FromResult(0);
+      }, context);
+
+      await _next(context);
+    }
+  }
+}
