c4d52c2bb4475451ceeb4fc3e7120addff6003e7
Doron Epstein <jimbeanzn@gmail.com>
2020-08-11 18:12:48 +0200

    fix: auth attribute not taking into account method specific roles

--- src/HulkOut.AspNetCore.Swashbuckle/OperationFilters/AuthResponsesOperationFilter.cs
+++ src/HulkOut.AspNetCore.Swashbuckle/OperationFilters/AuthResponsesOperationFilter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Net;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -10,16 +11,21 @@ namespace HulkOut.AspNetCore.Swashbuckle.OperationFilters
 	{
 		public void Apply(OpenApiOperation operation, OperationFilterContext context)
 		{
-			var authAttribute = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
-				.OfType<AuthorizeAttribute>()
-				.FirstOrDefault();
+			var authAttribute = context.MethodInfo.GetCustomAttributes(true)
+														.OfType<AuthorizeAttribute>()
+														.FirstOrDefault() ??
+													context.MethodInfo.DeclaringType?.GetCustomAttributes(true)
+														.OfType<AuthorizeAttribute>()
+														.FirstOrDefault();
 
 			if (authAttribute == null) return;
 
-			operation.Responses.Add(((int) HttpStatusCode.Unauthorized).ToString(),
-				new OpenApiResponse {Description = "Unauthorized"});
-			operation.Responses.Add(((int) HttpStatusCode.Forbidden).ToString(),
-				new OpenApiResponse {Description = "Forbidden"});
+			operation.Responses.Add(((int)HttpStatusCode.Unauthorized).ToString(),
+				new OpenApiResponse { Description = "Unauthorized" });
+			operation.Responses.Add(((int)HttpStatusCode.Forbidden).ToString(),
+				new OpenApiResponse { Description = "Forbidden" });
+
+			operation.Responses.Extensions.Add("x-allowed-roles", new OpenApiString(authAttribute.Roles));
 		}
 	}
 }
